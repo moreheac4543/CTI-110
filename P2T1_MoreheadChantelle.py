317df1b6d20e19f730370dd40965df0a950d8916
moreheac4543 <chantelle.morehead@outlook.com>
2020-03-02 18:26:16 -0500

    Add files via upload

---
+++ P2T1_MoreheadChantelle.py
@@ -0,0 +1,18 @@
+# Create a sales prediction and write a program where users to enter the projected amout of total sale
+# Date: Febuary 19th, 2020
+# CTI110 P2T1-Sales Prediction
+# Chantelle Morehead
+
+total_sales = float(input('Enter the projected sales: '))
+
+# Calculate the profit as 23 percent of total sales.
+earnings = total_sales * 0.23
+
+# Display the profit.
+print('The earning is $', format(earnings, ',.2f'))
+
+
+
+
+
+
